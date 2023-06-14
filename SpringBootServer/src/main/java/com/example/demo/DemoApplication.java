3acbccb5d382ebda978fe1810b21b0425b815bab
PlaIsMe <phongvulai96@gmail.com>
2023-06-14 14:11:00 +0700

    deploy spring boot

--- SpringBootServer/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBootServer/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.file.ConfigurationSource.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
